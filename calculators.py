64ce481484a5b7f5a3b316dc952b6f12be8b3f9d
jaekim3220 <jehyunkim9@gmail.com>
2023-05-22 17:15:51 +0900

    calculators.py 추가

---
+++ calculators.py
@@ -0,0 +1,17 @@
+'''a = 3
+b = 4
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)'''
+
+# 숫자->문자
+a = 10
+b = 20
+print(str(a)+str(b))
+
+c = 1234.5678
+print(int(c))
--- python1.py
+++ python1.py
@@ -1,3 +1,19 @@
-print("hello world")
-print("오늘 집에가서 반드시 복습하고 와라")
-print("안 그러면 진도 못 따라간다")
+# print("hello world")
+'''print("오늘 집에가서 반드시 복습하고 와라")
+print("안 그러면 진도 못 따라간다")'''
+
+a = 1
+b = '1'
+print(a)
+print(b)
+
+'''a = a+1
+print(a)
+b = b+a
+print(b)'''
+
+print(type(a))
+print(type(b))
+
+c = 2.1
+print(type(c))
