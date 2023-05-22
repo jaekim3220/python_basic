1eab8eac273e429be5dcfeb7590b4bf079580fae
jaekim3220 <jehyunkim9@gmail.com>
2023-05-22 17:29:25 +0900

    소숫점

--- calculators.py
+++ calculators.py
@@ -8,10 +8,24 @@ print(a/b)
 print(a//b)
 print(a%b)'''
 
-# 숫자->문자
+'''# 숫자->문자
 a = 10
 b = 20
 print(str(a)+str(b))
 
 c = 1234.5678
-print(int(c))
+print(int(c))'''
+
+a = round(1.23456)
+b = round(1.23456, 0)
+c = round(1.23456, 1)
+d = round(1.23456, 2)
+e = round(1.23456, 3)
+f = round(1.23456, 4)
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
